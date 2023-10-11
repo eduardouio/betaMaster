6923b3d098efdd0b3c1e70c0cb61f8047cbfaa1e
Eduardo Villota <eduardouio7@gmail.com>
2023-10-11 05:46:27 -0500

    settings

---
+++ src/homeSchool/settings/local.py
@@ -0,0 +1,15 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
