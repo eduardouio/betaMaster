6f2fa1205ea28693a63718369d3d00397d6e367c
Eduardo Villota <eduardouio7@gmail.com>
2023-10-17 10:12:56 -0500

    cambios compu

--- src/homeSchool/settings/local.py
+++ src/homeSchool/settings/local.py
@@ -13,3 +13,28 @@ LOGGING = {
         'level': 'DEBUG',
     },
 }
+
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+MY_SECRET_KEY = 'HOME_SCHOOL_098765redcvbnmk0987654edvbnp98765dvbnk097654323457l,mnbvftyjdk@#$%^&*EVILLOTA'
+
+app_database = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home_schooling',
+        'USER': 'eduardo',
+        'PASSWORD': 'elian.2011',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+    },
+    'TEST': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASE = app_database
