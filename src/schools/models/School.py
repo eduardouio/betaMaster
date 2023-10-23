0351094f786f27fbfcf0d4df98b31e88c3059e5e
Eduardo Villota <eduardouio7@gmail.com>
2023-10-23 15:28:03 -0500

    exenciion escuela

--- src/schools/models/School.py
+++ src/schools/models/School.py
@@ -1,30 +1,36 @@
 from django.db import models
 from accounts.models import CustomUserModel
+from common import BaseModel
 
 TYPE_SCHOOL = (
-    ('PUBLICO','PUBLICO')
-    ('PRIVADO','PRIVADO')
+    ('PUBLICO', 'PUBLICO'),
+    ('PRIVADO', 'PRIVADO')
 )
 
 
-class School(models.Model):
+class School(BaseModel):
     id_school = models.AutoField(primary_key=True)
     name = models.CharField(
         'Nombre Colegio',
         max_length=100,
         unique=True,
     )
-
     email = models.CharField(
         'Correo',
         max_length=100,
     )
-
     dni_collegue = models.CharField(
         'Ruc Colegio',
         max_length=50,
     )
-
+    state = models.CharField(
+        'Provincia',
+        max_length=100
+    )
+    city = models.CharField(
+        'Ciudad',
+        max_length=100
+    )
     address = models.CharField(
         'Dirección',
         max_length=100,
@@ -32,7 +38,13 @@ class School(models.Model):
         null=True,
         default=None,
     )
-
+    geolocation = models.CharField(
+        'Geolocalización',
+        max_length=500,
+        blank=True,
+        null=True,
+        default=None
+    )
     phone = models.CharField(
         'Teléfono',
         max_length=50,
@@ -40,28 +52,24 @@ class School(models.Model):
         null=True,
         default=None,
     )
-
     website = models.URLField(
         'Sitio Web',
         max_length=100,
         blank=True,
         null=True,
     )
-
     logo = models.ImageField(
         'Logo',
         upload_to='school/logos/',
         blank=True,
         null=True,
     )
-
     description = models.TextField(
         'Descripción',
         blank=True,
         null=True,
         default=None,
     )
-
     type_school = models.CharField(
         'Tipo Colegio',
         max_length=50,
@@ -69,10 +77,10 @@ class School(models.Model):
         null=True,
         default=None,
     )
-
     id_owner = models.ForeignKey(
-        CustomUserModel, 
+        CustomUserModel,
         models.RESTRICT
     )
 
-
+    def __str__(self) -> str:
+        return self.name
