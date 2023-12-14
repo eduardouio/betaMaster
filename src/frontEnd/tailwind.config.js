508472fa4fce86619dcd066cf1470300c45352e4
Eduardo Villota <eduardouio7@gmail.com>
2023-12-14 18:05:12 -0500

    Add custom theme to tailwind.config.js

--- src/frontEnd/tailwind.config.js
+++ src/frontEnd/tailwind.config.js
@@ -4,19 +4,36 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#009ddf",
+          "secondary": "#e0e0e0",
+          "accent": "#94c200",
+          "neutral": "#0c0902",
+          "base-100": "#fffdfc",
+          "info": "#008dff",
+          "success": "#00ffbc",
+          "warning": "#e66000",
+          "error": "#ff7b80",
+        },
+      },
+    ],
+  },
   darkMode: 'class',
   theme: {
     extend: {
-      backgroundColor:{
+      backgroundColor: {
         'primary': '#3490dc',
         'secondary': '#00f0f0',
         'tertiary': '#ffed4a',
       },
-      textColor:{
+      textColor: {
         'primary': '#3490dc',
         'secondary': '#ffed4a',
         'tertiary': '#ffed4a',
-      },fontFamily:{
+      }, fontFamily: {
         Montserrat: ['Montserrat', 'sans-serif'],
       },
     },
