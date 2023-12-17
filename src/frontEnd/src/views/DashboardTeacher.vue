04049b3c9a329c037a0275a9e3d001ca8ba0b34a
Eduardo Villota <eduardouio7@gmail.com>
2023-12-17 06:22:17 -0500

    dashboar pages

--- src/frontEnd/src/views/DashboardTeacher.vue
+++ src/frontEnd/src/views/DashboardTeacher.vue
@@ -1,7 +1,6 @@
 <script setup>
 import HeaderDashboard from '../components/dashboard/HeaderDashboard.vue';
 import SideBar from '../components/dashboard/SideBar.vue';
-
 import { RouterView } from 'vue-router';
 
 </script>
@@ -10,7 +9,6 @@ import { RouterView } from 'vue-router';
         <div
             class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
             <HeaderDashboard />
-            <h1>este es el dashboard bome de teacher</h1>
             <SideBar />
             <!--Contenido del dashboard-->
             <div class="h-full ml-14 mt-14 mb-10 md:ml-64 lg:pt-4 lg:pl-4">
