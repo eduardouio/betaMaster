eed492b42a24857f5e54c7489e6d2156ea1aacb8
Eduardo Villota <eduardouio7@gmail.com>
2023-12-18 16:53:26 -0500

    refactor

--- src/frontEnd/src/router/index.js
+++ src/frontEnd/src/router/index.js
@@ -1,9 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import DashBoardTeacher from '../views/DashBoardTeacher.vue'
-import TeacherProfile from '../components/dashboard/TeacherProfile.vue'
-import TeacherProfileForm from '../components/dashboard/TeacherProfileForm.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import DashBoardTeacher from '../views/DashBoardTeacher.vue';
+import HomeDashboard from '../components/dashboard/sections/HomeDashboard.vue';
+import SchoolsList from '../components/dashboard/sections/SchoolsList.vue';
+import StudentsList from '../components/dashboard/sections/StudentsList.vue';
+import TeacherProfile from '../components/dashboard/sections/TeacherProfile.vue';
+import TeacherProfileForm from '../components/dashboard/sections/TeacherProfileForm.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,17 +24,28 @@ const router = createRouter({
     },
     {
       path: '/dashboard-teacher',
-      name: 'dashboard-teacher',
       component: DashBoardTeacher,
       children:[
+        {
+          path: '',
+          component: HomeDashboard,
+        },
+        {
+          path: 'schools',
+          component: SchoolsList,
+        },
+        {
+          path: 'students',
+          component: StudentsList,
+        },
         {
           path: 'profile',
           component: TeacherProfile,
         },
         {
-          path: 'teacher-profile-form',
+          path: 'edit',
           component: TeacherProfileForm,
-        }
+        },
       ],
     }
   ]
