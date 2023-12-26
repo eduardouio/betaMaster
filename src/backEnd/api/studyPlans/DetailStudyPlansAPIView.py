9ceac6c85a285efd818c025ceaa0dd10b17e71d0
Eduardo Villota <eduardouio7@gmail.com>
2023-12-26 12:44:56 -0500

    refactor

--- src/backEnd/api/studyPlans/DetailStudyPlansAPIView.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.generics import RetrieveAPIView
-from studyPlans.models import StudyPlan
-from api.serializers import StudyPlanSerializer
-
-
-class DetailStudyPlansAPIView(RetrieveAPIView):
-    queryset = StudyPlan.objects.all()
-    serializer_class = StudyPlanSerializer
