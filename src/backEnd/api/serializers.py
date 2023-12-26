21509dff9ff3b01fba83b37d25afe5f339263bb8
Eduardo Villota <eduardouio7@gmail.com>
2023-12-26 11:18:29 -0500

    correccion en fields de los serializers

--- src/backEnd/api/serializers.py
+++ src/backEnd/api/serializers.py
@@ -16,45 +16,43 @@ class UserSerializer(serializers.ModelSerializer):
 class PersonalReferencesSerializer(serializers.ModelSerializer):
     class Meta:
         model = PersonalReferences
-        fields = ['__all__']
+        fields = '__all__'
 
 
 class BankAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = BankAccount
-        fields = ['__all__']
+        fields = '__all__'
+
 
 class ActiveCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = ActiveCourse
-        fields = ['__all__']
+        fields = '__all__'
 
 
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
-        fields = ['__all__']
+        fields = '__all__'
 
 
 class StudyPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudyPlan
-        fields = ['__all__']
+        fields = '__all__'
         depth = 1
 
 
 class studyPlanDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudyPlanDetail
-        fields = ['__all__']
+        fields = '__all__'
         depth = 1
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
-        fields = ['__all__']
+        fields = '__all__'
         depth = 1
-
-
-class
