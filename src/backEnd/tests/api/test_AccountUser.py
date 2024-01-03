42dd06fb1f1dff544c0679ffbfa6c89ee22f4867
Eduardo Villota <eduardouio7@gmail.com>
2024-01-03 17:16:22 -0500

    refactor, consolidamos en un solo archivo los test del mismo modelo

--- src/backEnd/tests/api/test_AccountUser.py
+++ src/backEnd/tests/api/test_AccountUser.py
@@ -19,3 +19,47 @@ class TestAccountUser(BaseTest):
         # verificamos la listas de referencias y cuentas
         assert isinstance(response['references'], list)
         assert isinstance(response['accounts'], list)
+
+
+@pytest.mark.django_db
+class TestListStudyPlans(BaseTest):
+
+    @pytest.fixture
+    def url(self):
+        return reverse('api:api-users-by-role-list', kwargs={'role_name': 'student'})
+
+    def test_not_authorized(self, client_guest, url):
+        # sobreescribimos el metodo para que no de error
+        pass
+
+    def test_list_users_students(self, client_logged, url):
+        url = url.replace('role_name', 'student')
+        response = client_logged.get(url)
+        assert response.status_code == 200
+        response = response.json()
+        assert isinstance(response, dict)
+        assert response['results'][0]['role'] == 'student'
+
+    def test_list_users_school(self, client_logged, url):
+        url = url.replace('student', 'school')
+        response = client_logged.get(url)
+        assert response.status_code == 200
+        response = response.json()
+        assert isinstance(response, dict)
+        assert response['results'][0]['role'] == 'school'
+        # verificamos que contiene la paginacion
+        assert response['results']
+        assert response['count']
+        assert response['next']
+
+    def test_list_users_teacher(self, client_logged, url):
+        url = url.replace('student', 'teacher')
+        response = client_logged.get(url)
+        assert response.status_code == 200
+        response = response.json()
+        assert isinstance(response, dict)
+        assert response['results'][0]['role'] == 'teacher'
+        # verificamos que contiene la paginacion
+        assert response['results']
+        assert response['count']
+        assert response['next']
