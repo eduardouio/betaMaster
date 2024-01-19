87dd7bf9915618d909c7af09964c6b3c490e11d3
Eduardo Villota <eduardouio7@gmail.com>
2024-01-19 11:30:23 -0500

    obtenemos los datos del usuario

--- src/frontEnd/src/views/DashboardTeacher.vue
+++ src/frontEnd/src/views/DashboardTeacher.vue
@@ -1,13 +1,43 @@
 <script setup>
-import HeaderDashboard from '../components/dashboard/HeaderDashboard.vue';
-import SideBar from '../components/dashboard/SideBar.vue';
+import {onMounted } from 'vue';
+import {useStore} from 'vuex';
 import { RouterView } from 'vue-router';
+import serverConfigData from '../config';
+
+import SideBar from '../components/dashboard/SideBar.vue';
+import HeaderDashboard from '../components/dashboard/HeaderDashboard.vue';
+import Loader from '../components/generics/Loader.vue';
+
+const store = useStore();
+const showLoader = false;
+
+onMounted(() => {
+    console.log('montado dashboard');
+    getUserData();
+});
+
+// recuperamos los datos del usuario y lo colocamos en el store
+async function getUserData(){
+    let url = serverConfigData.urls.getUser.replace(
+        '{idUser}', serverConfigData.idUser
+    );
+    let response = await fetch(url, {
+        method: 'GET',
+        headers: serverConfigData.headers
+    });
+    
+    if (response.status !=200){
+        alert('Error al obtener los datos del usuario');
+    }else{
+        store.commit('setUserData', await response.json());
+    }
+}
 </script>
 
 <template>
     <div>
-        <div
-            class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
+    <Loader v-if="showLoader"/>
+    <div v-if="!showLoader" class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
             <HeaderDashboard />
             <SideBar />
             <!--Contenido del dashboard-->
