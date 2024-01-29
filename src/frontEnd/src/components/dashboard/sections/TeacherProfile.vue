a9228667d08cc29ebec9268739b250bb2d55ea7f
Eduardo Villota <eduardouio7@gmail.com>
2024-01-29 16:48:26 -0500

    eliminamos variables de traduccion

--- src/frontEnd/src/components/dashboard/sections/TeacherProfile.vue
+++ src/frontEnd/src/components/dashboard/sections/TeacherProfile.vue
@@ -2,7 +2,8 @@
 import { RouterLink } from 'vue-router';
 import { useStore } from 'vuex';
 import { 
-    CheckBadgeIcon, CogIcon, NewspaperIcon, MapPinIcon, FolderArrowDownIcon
+    CheckBadgeIcon, CogIcon, NewspaperIcon, MapPinIcon, FolderArrowDownIcon,
+    XMarkIcon, CheckIcon, PencilSquareIcon
 } from '@heroicons/vue/24/outline';
 import serverConfigData from '../../../config.js';
 
@@ -11,11 +12,6 @@ import { computed, reactive } from 'vue';
 
 const store = useStore();
 let userData = store.getters.getUserData;
-let roleUser = {
-    teacher: 'Docente Educativo',
-    student: 'Estuidante',
-    school: 'Institución Educativa',
-};
 
 const tabList = reactive({
     personal_data: true,
@@ -35,27 +31,6 @@ const geolocation = computed(() => {
     let url = `https://www.google.com/maps/?q=${coordinates[0]},${coordinates[1]}`;
     return url;
 });
-
-const civil_status = {
-    single: 'Soltero',
-    married: 'Casado',
-    divorced: 'Divorciado',
-    widowed: 'Viudo',
-    separated: 'Separado',
-    free_union: 'Unión Libre',
-    other: 'Otro',
-};
-
-const level_education = {
-    primary: 'Primaria',
-    secundary: 'Secundaria',
-    superior: 'Superior',
-    Postgrado: 'Postgrado',
-    master: 'Master',
-    doctor: 'Doctorado',
-    other: 'Otro',
-};
-
 
 const formatDate = ((my_date) => {
     if (!my_date) {
@@ -129,7 +104,8 @@ const timeLapsed = ((my_date, years = true) => {
                 Informacion Bancarias
             </a>
         </div>
-        <div class="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
+        <!--tab profile-->
+        <div v-if="tabList.personal_data" class="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
             <div class="w-full flex flex-col 2xl:w-3/5">
                 <div class="flex-1 bg-white rounded-lg shadow-xl p-7">
                     <ul class="text-gray-700">
@@ -196,7 +172,7 @@ const timeLapsed = ((my_date, years = true) => {
                         </li>
                         <li class="flex border-b py-2">
                             <span class="font-bold w-2/5">Nacionalidad:</span>
-                            <span class="text-gray-700">{{ userData.nationality }} <small class="text-gray-300">|</small> {{ civil_status[userData.civil_status] }}</span>
+                            <span class="text-gray-700">{{ userData.nationality }} <small class="text-gray-300">|</small> {{ userData.civil_status }}</span>
                         </li>
                         <li class="flex border-b py-2">
                             <span class="font-bold w-2/5">Nro Cedula:</span>
@@ -204,7 +180,7 @@ const timeLapsed = ((my_date, years = true) => {
                         </li>
                         <li class="flex border-b py-2">
                             <span class="font-bold w-2/5">Nivel Educativo:</span>
-                            <span class="text-gray-700">{{ level_education[userData.level_education] }}</span>
+                            <span class="text-gray-700">{{ userData.level_education }}</span>
                         </li>
                         <li class="flex border-b py-2">
                             <span class="font-bold w-2/5">Discapacidad:</span>
@@ -220,5 +196,138 @@ const timeLapsed = ((my_date, years = true) => {
                 </div>
             </div>
         </div>
+        <!--/tab profile-->
+        <!--tab references-->
+        <div v-if="tabList.references" class="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
+  <table class="table border">
+    <!-- head -->
+    <thead>
+      <tr class="bg-gradient-to-t from-slate-500 to-slate-400 text-white text-center">
+        <th>#</th>
+        <th>Tipo</th>
+        <th>Empresa</th>
+        <th>Contacto</th>
+        <th>Inicio</th>
+        <th>Fin</th>
+        <th>Estado</th>
+        <th><CogIcon class="w-5 h-5"/></th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr v-for="(reference, idx) in userData.references" :key="reference.id_reference" class="hover:bg-yellow-50">
+            <td>{{ idx+1 }}</td>
+            <td>{{ reference.type }}</td>
+            <td>{{ reference.enterprise }}</td>
+            <td>
+                {{ reference.name_contact }} 
+                <small class="badge bg-cyan-50">{{ reference.relationship }}</small>
+            </td>
+            <td>{{ reference.start_date }}</td>
+            <td>{{ reference.end_date }}</td>
+            <td class="text-center">
+                <CheckIcon v-if="reference.is_verified" class="w-5 h-5 inline-block text-success" />
+                <XMarkIcon v-else class="w-5 h-5 inline-block text-error" />
+            </td>
+            <td>
+                <PencilSquareIcon class="w-5 h-5 inline-block text-primary" />
+            </td>
+        </tr>
+    </tbody>
+  </table>
+        </div>
+        <!--/tab references-->
+        <!--tab bank data-->
+        <div v-if="tabList.bank_data" class="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
+            <div class="w-full flex flex-col 2xl:w-3/5">
+                <div class="flex-1 bg-white rounded-lg shadow-xl p-7">
+                    <ul class="text-gray-700">
+                        <li class="flex border-y py-2"> 
+                            <span class="font-bold w-2/5">Nombres:</span>
+                            <span class="text-gray-700">
+                                <div class="tooltip" data-tip="Descargar Hoja de Vida" v-if="userData.cv"> 
+                                    <a :href="userData.cv">
+                                        <FolderArrowDownIcon class="w-5 h-5 inline-block text-info"/>
+                                    </a>
+                                </div>
+                                {{ userData.first_name }} {{ userData.last_name }}
+                                <small v-if="userData.profesion" class="badge badge-outline badge-neutral">{{ userData.profesion }}</small>
+                                
+                            </span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">F Nacimiento:</span>
+                            <span class="text-gray-700">{{ formatDate(userData.date_of_birth) }} <small
+                                    class="text-slate-600">({{ timeLapsed(userData.date_of_birth) }})</small></span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Joined:</span>
+                            <span class="text-gray-700">{{ formatDate(userData.date_joined) }}<small
+                                    class="text-slate-600">({{ timeLapsed(userData.date_joined,
+                                        years = false) }})</small></span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Celular:</span>
+                            <span class="text-gray-700">{{ userData.phone }} <span class="text-gray-300">|</span> {{ userData.phone_2 }}</span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Email:</span>
+                            <div class="tooltip" :data-tip="userData.is_confirmed_mail?'Correo Confirmado':'Sin Confirmar'" :class="!userData.is_confirmed_mail?'tooltip-error':''">
+                            <span class="text-gray-700">{{ userData.email }}
+                                    <CheckBadgeIcon v-if="userData.is_confirmed_mail" class="w-5 h-5 inline-block text-success" />
+                                </span>
+                            </div>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Ubicación:</span>
+                            <span class="text-gray-700">{{ userData.state }}, {{ userData.city }} <br> {{ userData.address
+                            }}</span>
+                        </li>
+                        <li class="flex items-center border-b py-2 space-x-2">
+                            <span class="font-bold w-2/5">Mis Redes:</span>
+                            <SocialIcon :url="userData.url_facebook" :icon="'facebook'" />
+                            <SocialIcon :url="userData.url_linkedin" :icon="'linkedin'" />
+                            <SocialIcon :url="userData.url_instagram" :icon="'instagram'" />
+                            <SocialIcon :url="userData.url_twiter" :icon="'twitter'" />
+                        </li>
+                    </ul>
+                </div>
+            </div>  
+            <div class="w-full flex flex-col 2xl:w-3/5">
+                <div class="flex-1 bg-white rounded-lg shadow-xl p-7">
+                    <ul class="text-gray-700">
+                        <li class="flex items-center border-y py-2">
+                            <span class="font-bold w-2/5">Ubicación:</span>
+                            <section class="text-gray-700 flex">
+                                <a :href="geolocation" target="_blank"><MapPinIcon class="w-6 h-6 text-primary" /></a>
+                                {{ userData.state }}, {{ userData.city}}, {{ userData.address }}
+                            </section>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Nacionalidad:</span>
+                            <span class="text-gray-700">{{ userData.nationality }} <small class="text-gray-300">|</small> {{ userData.civil_status }}</span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Nro Cedula:</span>
+                            <span class="text-gray-700">{{ userData.dni_number}}</span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Nivel Educativo:</span>
+                            <span class="text-gray-700">{{ userData.level_education}}</span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Discapacidad:</span>
+                            <span class="text-gray-700">{{ userData.have_disability?'SI':'NO' }} 
+                                <small v-if="userData.have_disability" class="text-info">{{ userData.type_disability }} {{ userData.disability_persent }}% {{ userData.card_conadis }}</small>
+                            </span>
+                        </li>
+                        <li class="flex border-b py-2">
+                            <span class="font-bold w-2/5">Último Acceso:</span>
+                            <span class="text-gray-700">{{ formatDate(userData.last_login) }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!--/tab bank data-->
     </div>
 </template>
