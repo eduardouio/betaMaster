b4d0374e710bb481e1bd9ba5e4ad364ff7353b5c
Eduardo Villota <eduardouio7@gmail.com>
2024-01-29 17:54:03 -0500

    agregamos los estados para el dashobard y la seccion de estudiantes

--- src/frontEnd/store.js
+++ src/frontEnd/store.js
@@ -2,21 +2,41 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-        userData: null
+        userData: null,
+        userStudents: null,
+        dashboardData: null,
     },
     mutations: {
         setUserData(state, userData) {
             state.userData = userData;
+        },
+        setUserStudents(state, userStudents) {
+            state.userStudents = userStudents;
+        },
+        setDashboardData(state, dashboardData) {
+            state.dashboardData = dashboardData;
         }
     },
     actions: {
         setUserData({ commit }, userData) {
             commit('setUserData', userData);
+        },
+        setUserStudents({ commit }, userStudents) {
+            commit('setUserStudents', userStudents);
+        },
+        setDashboardData({ commit }, dashboardData) {
+            commit('setDashboardData', dashboardData);
         }
     },
     getters: {
         getUserData(state) {
             return state.userData;
+        },
+        getUserStudents(state) {
+            return state.userStudents;
+        },
+        getDashboardData(state) {
+            return state.dashboardData;
         }
     }
 });
