9238516c5efc6bcbb34b903436ebf2c2796c1cbb
Eduardo Villota <eduardouio7@gmail.com>
2024-01-30 17:48:46 -0500

    home

--- src/frontEnd/src/components/dashboard/sections/HomeDashboard.vue
+++ src/frontEnd/src/components/dashboard/sections/HomeDashboard.vue
@@ -5,9 +5,19 @@ import TableCandidates from '@//components/dashboard/TableCandidates.vue';
 </script>
 <template>
     <div>
-        <div class="grid grid-cols-1 grid-cols-2 gap-10 ">
-            <CardIndicator />
-            <CardIndicator />
+        <div class="grid items-center grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 ">
+            <CardIndicator
+                :nameCard="'Cursos'"
+                :totalCard="12"
+             />
+             <CardIndicator
+                :nameCard="'Estudiantes'"
+                :totalCard="34"
+             />
+             <CardIndicator
+                :nameCard="'Colegios'"
+                :totalCard="23"
+             />
         </div>
         <div class="grid grid-cols-1 lg:grid-cols-2 p-4 gap-4">
             <TableSchools />
