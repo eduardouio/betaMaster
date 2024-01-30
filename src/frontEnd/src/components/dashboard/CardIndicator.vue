90a1c5b592bc60f35b772393ef47cc01cff272fc
Eduardo Villota <eduardouio7@gmail.com>
2024-01-30 17:49:23 -0500

    cards del home

--- src/frontEnd/src/components/dashboard/CardIndicator.vue
+++ src/frontEnd/src/components/dashboard/CardIndicator.vue
@@ -1,25 +1,35 @@
 <script setup>
 import { UserGroupIcon } from '@heroicons/vue/24/outline';
 import { BuildingLibraryIcon } from '@heroicons/vue/24/outline';
-</script>
+import { BookOpenIcon } from '@heroicons/vue/24/outline';
 
+const props = defineProps({
+    nameCard: {
+        type: String,
+        required: true
+    },
+    totalCard: {    
+        type: Number,
+        required: true
+    }
+});
+
+</script>
 <template>
     <div>
-        <div class="px-6 py-6 bg-gray-100 border border-gray-300 rounded-lg shadow-xl">
+        <div class="px-6 py-6 border border-blue-200 rounded-lg shadow-md">
             <div class="flex items-center justify-between">
-                <span class="font-bold text-sm text-indigo-600">Estudiantes</span>
-                <span
-                    class="text-xs text-white bg-red-600 hover:bg-red-500 hover:text-gray-200 px-2 py-1 rounded-lg transition duration-200 cursor-default">
-                    2 nuevos
-                </span>
+                <span class="font-bold text-sm text-primary">{{ nameCard }}</span>
             </div>
             <div class="flex items-center justify-between mt-6">
                 <div>
-                    <UserGroupIcon class="w-12 h-12"/>
+                    <UserGroupIcon v-if="nameCard==='Estudiantes'" class="w-12 h-12 text-info"/>
+                    <BuildingLibraryIcon v-if="nameCard==='Colegios'" class="w-12 h-12 text-info"/>
+                    <BookOpenIcon v-if="nameCard==='Cursos'" class="w-12 h-12 text-info"/>
                 </div>
                 <div class="flex flex-col">
                     <div class="flex items-end">
-                        <span class="text-2xl 2xl:text-3xl font-bold">12 Estudiantes</span>
+                        <span class="text-2xl 2xl:text-3xl font-bold text-gray-600 uppercase">{{ totalCard }} {{nameCard}}</span>
                     </div>
                 </div>
             </div>
