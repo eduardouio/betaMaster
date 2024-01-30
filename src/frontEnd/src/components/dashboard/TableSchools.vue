a4d60570119a053cd7fe7664ae9e3554a832baf4
Eduardo Villota <eduardouio7@gmail.com>
2024-01-30 17:49:00 -0500

    dashboard teacher

--- src/frontEnd/src/components/dashboard/TableSchools.vue
+++ src/frontEnd/src/components/dashboard/TableSchools.vue
@@ -1,95 +1,97 @@
+<script setup>
+import { useStore } from 'vuex';
+import { onMounted, ref } from 'vue';
+import serverConfigData from '@/config';
+
+let showLoader = ref(true);
+const store = useStore();
+let dashboardData = store.state.dashboardData;
+console.dir(dashboardData.school);
+</script>
 <template>
     <div
-                        class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
-                        <div class="rounded-t mb-0 px-0 border-0">
-                            <div class="flex flex-wrap items-center px-4 py-2">
-                                <div class="relative w-full max-w-full flex-grow flex-1">
-                                    <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Mis Colegios Asignados     
-                                        <span class="bg-red-600 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded-full">4</span>    
-                                    </h3>
+        class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+        <div class="rounded-t mb-0 px-0 border-0">
+            <div class="flex flex-wrap items-center px-4 py-2">
+                <div class="relative w-full max-w-full flex-grow flex-1">
+                    <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Mis Colegios Asignados
+                        <span class="bg-red-600 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded-full">4</span>
+                    </h3>
+                </div>
+                <div class="relative w-full max-w-full flex-grow flex-1 text-right">
+                    <button
+                        class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button">Ver Todo</button>
+                </div>
+            </div>
+            <div class="block w-full overflow-x-auto">
+                <table class="items-center w-full bg-transparent border-collapse">
+                    <thead>
+                        <tr>
+                            <th
+                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Colegio</th>
+                            <th
+                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Estudiantes</th>
+                            <th
+                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
+                                Ciudad
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-gray-700 dark:text-gray-100">
+                            <th
+                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                Colegio General Juan Flores</th>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                3</td>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                <div class="flex items-center">
+                                    <span class="mr-2">QUITO</span>
                                 </div>
-                                <div class="relative w-full max-w-full flex-grow flex-1 text-right">
-                                    <button
-                                        class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button">Ver Todo</button>
+                            </td>
+                        </tr>
+                        <tr class="text-gray-700 dark:text-gray-100">
+                            <th
+                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                Colegio Nacional Montufar</th>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                3</td>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                <div class="flex items-center">
+                                    <span class="mr-2">QUITO</span>
                                 </div>
-                            </div>
-                            <div class="block w-full overflow-x-auto">
-                                <table class="items-center w-full bg-transparent border-collapse">
-                                    <thead>
-                                        <tr>
-                                            <th
-                                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                                Colegio</th>
-                                            <th
-                                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                                Estudiantes</th>
-                                            <th
-                                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
-                                                Ciudad
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="text-gray-700 dark:text-gray-100">
-                                            <th
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                                Colegio General Juan Flores</th>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                3</td>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <div class="flex items-center">
-                                                    <span class="mr-2">QUITO</span>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr class="text-gray-700 dark:text-gray-100">
-                                            <th
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                                Colegio Nacional Montufar</th>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                3</td>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <div class="flex items-center">
-                                                    <span class="mr-2">QUITO</span>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr class="text-gray-700 dark:text-gray-100">
-                                            <th
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                                Colego Gran Bretaña</th>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                3</td>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <div class="flex items-center">
-                                                    <span class="mr-2">QUITO</span>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr class="text-gray-700 dark:text-gray-100">
-                                            <th
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                                Colegio General Juan Flores</th>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                3</td>
-                                            <td
-                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                                <div class="flex items-center">
-                                                    <span class="mr-2">QUITO</span>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+                            </td>
+                        </tr>
+                        <tr class="text-gray-700 dark:text-gray-100">
+                            <th
+                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                Colego Gran Bretaña</th>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                3</td>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                <div class="flex items-center">
+                                    <span class="mr-2">QUITO</span>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="text-gray-700 dark:text-gray-100">
+                            <th
+                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                Colegio General Juan Flores</th>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                3</td>
+                            <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                <div class="flex items-center">
+                                    <span class="mr-2">QUITO</span>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </template>
--- src/frontEnd/src/views/DashboardTeacher.vue
+++ src/frontEnd/src/views/DashboardTeacher.vue
@@ -1,28 +1,56 @@
 <script setup>
-import {onMounted } from 'vue';
-import {useStore} from 'vuex';
 import { RouterView } from 'vue-router';
-import serverConfigData from '../config';
+import { onMounted, ref, shallowReactive } from 'vue';
+import { useStore } from 'vuex';
+import serverConfigData from '@/config';
+import Footer from '@/components/Footer.vue';
+import Loader from '@/components/generics/Loader.vue';
+import SideBar from '@/components/dashboard/SideBar.vue';
+import HeaderDashboard from '@/components/dashboard/HeaderDashboard.vue';  
 
-import SideBar from '../components/dashboard/SideBar.vue';
-import HeaderDashboard from '../components/dashboard/HeaderDashboard.vue';
-import Loader from '../components/generics/Loader.vue';
-
+let showLoader = ref(true);
 const store = useStore();
+let dashboardData = {};
+
+onMounted(() => {
+    document.title = 'Cargando Datos...';
+    loadData();
+});
 
 
+// cargamos los datos del dashboard
+async function loadData(){
+    let url = serverConfigData.urls.teacherData.replace(
+        '{idUser}', serverConfigData.idUser
+    );
+    console.dir(url);
+    let response = await fetch(
+        url,{
+        method: 'GET',
+        headers: serverConfigData.headers
+    });
 
-
+    if(response.status != 200){
+        console.log('Error al cargar los datos del dashboard');
+        console.log(response);
+    }else{        
+        store.commit('setDashboardData', await response.json());
+        dashboardData= await store.state.dashboardData;
+        document.title = 'Resumen | Dashboard';
+        showLoader.value = false;
+    }
+}
 </script>
-
 <template>
-    <div>
+    <Loader v-if="showLoader" />
+    <div v-else>
     <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
             <HeaderDashboard />
             <SideBar />
             <!--Contenido del dashboard-->
-            <div class="h-full ml-14 mt-14 mb-10 md:ml-64 lg:pt-4 lg:pl-4">
+            <div class="h-full ml-14 mt-10 mb-10 md:ml-64 lg:pt-4 lg:pl-4 mr-2">
                 <RouterView />
+                <Footer />
             </div>
             <!--/Contenido del dashboard-->
         </div>
