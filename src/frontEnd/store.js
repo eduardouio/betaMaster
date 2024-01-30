1e043ac769afb2e3574bb386b8c830c57a4656de
Eduardo Windows <eduardouio7@gmail.com>
2024-01-29 22:43:34 -0500

    mantenemos solo lo que usamos

--- src/frontEnd/store.js
+++ src/frontEnd/store.js
@@ -3,15 +3,11 @@ import { createStore } from "vuex";
 export default createStore({
     state: {
         userData: null,
-        userStudents: null,
         dashboardData: null,
     },
     mutations: {
         setUserData(state, userData) {
             state.userData = userData;
-        },
-        setUserStudents(state, userStudents) {
-            state.userStudents = userStudents;
         },
         setDashboardData(state, dashboardData) {
             state.dashboardData = dashboardData;
@@ -21,9 +17,6 @@ export default createStore({
         setUserData({ commit }, userData) {
             commit('setUserData', userData);
         },
-        setUserStudents({ commit }, userStudents) {
-            commit('setUserStudents', userStudents);
-        },
         setDashboardData({ commit }, dashboardData) {
             commit('setDashboardData', dashboardData);
         }
@@ -31,9 +24,6 @@ export default createStore({
     getters: {
         getUserData(state) {
             return state.userData;
-        },
-        getUserStudents(state) {
-            return state.userStudents;
         },
         getDashboardData(state) {
             return state.dashboardData;
