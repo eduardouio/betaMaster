8770d6b5c26222591197d1098f302e7e673d5eb1
Eduardo Villota <eduardouio7@gmail.com>
2024-01-30 16:27:28 -0500

    quitamos el footer de la app para asignarlo en cada vista

--- src/frontEnd/src/App.vue
+++ src/frontEnd/src/App.vue
@@ -2,11 +2,9 @@
 import { RouterLink } from 'vue-router';
 import HomeView from './views/HomeView.vue';
 import RegisterView from './views/RegisterView.vue';
-import Footer from './components/Footer.vue';
 </script>
 <template>
     <div>
         <RouterView />
-        <Footer/>
     </div>
 </template>
