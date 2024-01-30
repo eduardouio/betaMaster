28a40f99d9dd927ad569a64231bd12362c90db0b
Eduardo Villota <eduardouio7@gmail.com>
2024-01-30 16:25:34 -0500

    centramos y agrandamos el loader

--- src/frontEnd/src/components/generics/Loader.vue
+++ src/frontEnd/src/components/generics/Loader.vue
@@ -1,3 +1,6 @@
 <template>
-    <span class="loading loading-spinner loading-lg text-teal-400"></span>
+    <div class="flex flex-col items-center">
+        <span class="loading loading-spinner text-sky-500 w-1/4 h-auto mx-auto my-auto"></span>
+        <span class="text-red-800 text-3xl">cargando...</span>
+    </div>
 </template>
