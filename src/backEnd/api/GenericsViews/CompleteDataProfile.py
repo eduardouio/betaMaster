a31359b93e341f64c5db9a7a0ad7366912691af4
Eduardo Villota <eduardouio7@gmail.com>
2024-02-01 15:51:27 -0500

    generamos un solo arreglo para los datos

--- src/backEnd/api/GenericsViews/CompleteDataProfile.py
+++ src/backEnd/api/GenericsViews/CompleteDataProfile.py
@@ -8,7 +8,34 @@ from django.shortcuts import get_object_or_404
 # /api/user/teacher-data/{idUser}/ -> api-teacher-data
 class TeacherDataApiView(APIView):
     """return all active courses by user"""
+
     def get(self, request, pk):
         user = get_object_or_404(CustomUserModel, pk=pk)
         serializer = CompleteDataForTeacherSerializer(user)
-        return Response(serializer.data)
+        data = []
+
+        for active_course in serializer.data['active_courses']:
+            row = {
+                'active_course': {
+                    'id_active_courses': active_course['id_active_courses'],
+                    'id_student': active_course['student'],
+                    'id_school': active_course['id_school'],
+                    'id_teacher': pk,
+                    'period': active_course['period'],
+                    'state': active_course['state'],
+                },
+                'school': None,
+                'student': None,
+            }
+
+            for school in serializer.data['school']:
+                if active_course['id_school'] == school['id']:
+                    row['school'] = school
+
+            for student in serializer.data['students']:
+                if active_course['student'] == student['id']:
+                    row['student'] = student
+
+            data.append(row)
+
+        return Response(data)
