a202b963b0fc707cc3ab054434f7463c10ea956e
Eduardo Villota <eduardouio7@gmail.com>
2024-02-02 11:53:56 -0500

    tabla paginada completa

--- src/frontEnd/src/components/dashboard/TableSchools.vue
+++ src/frontEnd/src/components/dashboard/TableSchools.vue
@@ -1,35 +1,78 @@
 <script setup>
 import { useStore } from 'vuex';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
+import 
+    { 
+        ChevronDoubleLeftIcon,
+        ChevronDoubleRightIcon,
+        ChevronRightIcon,
+        ChevronLeftIcon 
+    } 
+from '@heroicons/vue/24/outline';
 import serverConfigData from '@/config';
-import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon, ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/24/outline';
 
 const store = useStore();
 let dashboardData = store.state.dashboardData;
 let paginatedData = ref([]);
-
 let currentPage = ref(1);
 let perPage = ref(10);
-let offset = ref((currentPage.value - 1) * perPage.value);
-let paginatedItems = ref(dashboardData.slice(offset.value).slice(0, perPage.value));
+let filter = ref('');
+let pages = ref(Math.ceil(dashboardData.length / perPage.value));
 
+onMounted(() => {paginateContent(dashboardData);});
 
-onMounted(() => {
-    paginatedData.value = paginateContent(dashboardData);
+// miramos la cantidad de registros por pagina
+watch(perPage, (newVal, oldVal) => {
+    currentPage.value = 1;
+    pages.value = Math.ceil(dashboardData.length / perPage.value);
+    paginateContent(dashboardData);
 });
 
-function paginateContent(data) {
-    let paginatedData = [];
-    let page = 1;
-    let perPage = 10;
-    let offset = (page - 1) * perPage;
-    let paginatedItems = data.slice(offset).slice(0, perPage);
-    for (let i = 0; i < paginatedItems.length; i++) {
-        paginatedData.push(paginatedItems[i]);
+// miramos la pagina actual
+watch(currentPage, (newVal, oldVal) => {
+    if (newVal === -1) {
+        currentPage.value = 1;
+    } else if (newVal === -2) {
+        currentPage.value = pages.value;
     }
-    return paginatedData;
+    if (currentPage.value > pages.value) {
+        currentPage.value = pages.value;
+    }
+    if (currentPage.value < 1) {
+        currentPage.value = 1;
+    }
+    paginateContent(dashboardData);
+});
+
+// miramos si tenemos el filtro
+watch(filter, (newVal, oldVal) => {
+    currentPage.value = 1;
+    paginateContent(dashboardData, newVal);
+});
+
+
+// paginacion de datos
+function paginateContent(data, filter='') {
+    if (filter) {
+        data = filterData(data, filter);
+    }
+    let start = (currentPage.value - 1) * perPage.value;
+    let paginatedItems = data.slice(start, start + perPage.value);
+    paginatedData.value = paginatedItems;
 }
 
+// filtro de datos
+function filterData(data, filter) {
+    let filteredData = data.filter((item) => {
+        return item.student.first_name.toLowerCase().includes(filter.toLowerCase()) || 
+        item.student.last_name.toLowerCase().includes(filter.toLowerCase()) || 
+        item.school.name.toLowerCase().includes(filter.toLowerCase()) || 
+        item.active_course.period.toLowerCase().includes(filter.toLowerCase()) || 
+        item.active_course.state.toLowerCase().includes(filter.toLowerCase()) || 
+        item.student.city.toLowerCase().includes(filter.toLowerCase());
+    });
+    return filteredData;
+}
 
 </script>
 <template>
@@ -38,11 +81,12 @@ function paginateContent(data) {
         <div class="rounded-t mb-0 px-0 border-0">
             <div class="flex flex-wrap items-center px-4 py-2">
                 <div class="relative w-full max-w-full flex-grow flex-1">
-                    <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Mis Estudiantes
+                    <h3 class="text-gray-900">Mis Estudiantes 
                     </h3>
                 </div>
                 <div class="relative w-full max-w-full flex-grow flex-1 text-right">
-                    <button class="text-sm text-primary btn btn-sm btn-outline">Ver Todo</button>
+                    <input type="text" class="input input-bordered input-xs" placeholder="buscar" v-model="filter">
+
                 </div>
             </div>
             <div class="block w-full overflow-x-auto">
@@ -58,8 +102,8 @@ function paginateContent(data) {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="row,idx in paginateContent" class=" hover:bg-yellow-50" :key="row">
-                            <td class="pb-0 pl-1">{{idx+1}}</td>
+                        <tr v-for="row,idx in paginatedData" class=" hover:bg-yellow-50" :key="row">
+                            <td class="pb-0 pl-1">{{ (perPage*(currentPage-1)) + idx+1}}</td>
                             <td class="pb-0 pl-1">{{row.student.first_name}} {{row.student.last_name}}</td>
                             <td class="pb-0 pl-1">{{row.school.name}}</td>
                             <td class="pb-0 pl-1">{{row.active_course.period}}</td>
@@ -68,12 +112,45 @@ function paginateContent(data) {
                         </tr>
                     </tbody>
                 </table> 
-                <section class="flex gap-2 p-2">
-                    <button class="btn btn-xs btn-outline"><ChevronDoubleLeftIcon class="w-4 h-4"/></button>
-                    <button class="btn btn-xs btn-outline"><ChevronLeftIcon class="h-4 w-4"/></button>
-                    <button class="btn btn-xs btn-outline"><ChevronRightIcon class="h-4 w-4"/></button>
-                    <button class="btn btn-xs btn-outline"><ChevronDoubleRightIcon class="h-4 w-4"/></button>
-                </section>
+                <br/>
+                <hr>
+                <div class="flex flex-wrap items-center px-4 py-2 bg-slate-100">
+                <div class="relative w-full max-w-full flex-grow flex-1 flex gap-1">
+                    <button 
+                        @click="currentPage =-1"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronDoubleLeftIcon class="w-4 h-4"/>
+                    </button>
+                    <button 
+                        @click="currentPage--"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronLeftIcon class="h-4 w-4"/>
+                    </button>
+                    <button 
+                        @click="currentPage++"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronRightIcon class="h-4 w-4"/>
+                    </button>
+                    <button 
+                        @click="currentPage=-2"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronDoubleRightIcon class="h-4 w-4"/>
+                    </button> 
+                </div>
+                <div class="relative text-xs">
+                    Página 1 de {{ pages }} - Mostrando {{ perPage }} de {{ paginatedData.length }} Registros Encontrados
+                </div>
+                <div class="relative w-full max-w-full flex-grow flex-1 text-right">
+                    <span class="text-xs">Elementos por Página </span>
+                    <select class="select select-bordered select-xs" v-model="perPage">
+                        <option value="10">10</option>
+                        <option value="30">30</option>
+                        <option value="60">60</option>
+                        <option value="100">100</option>        
+                    </select>
+                </div>
+            </div>
+                <hr class="p-2">
             </div>
         </div>
     </div>
