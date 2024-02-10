6558f5f72de33088ff57a88519e45f8730b1b08e
Eduardo Villota <eduardouio7@gmail.com>
2024-02-10 16:33:55 -0500

    tabla de estuduantes colocada correctamente

--- src/frontEnd/src/components/dashboard/TableStudents.vue
+++ src/frontEnd/src/components/dashboard/TableStudents.vue
@@ -1,186 +1,175 @@
+<script setup>
+import { useStore } from 'vuex';
+import { onMounted, ref, watch, defineEmits } from 'vue';
+import 
+    { 
+        ChevronDoubleLeftIcon,
+        ChevronDoubleRightIcon,
+        ChevronRightIcon,
+        ChevronLeftIcon 
+    } 
+from '@heroicons/vue/24/outline';
+import serverConfigData from '@/config';
+
+// defimimos los emits para actualizar el modal
+const emits = defineEmits(['idStudent']);
+
+function emitIdStudent(id){
+    emits('idStudent', id);
+}
+
+const classsStatus = {
+    'POR INICIAR': 'text-sky-900 md:w-1/2',
+    'EN PROCESO': 'text-green-900 md:w-1/2',
+    'FINALIZADO': 'text-red-900 md:w-1/2'
+};
+
+const store = useStore();
+let dashboardData = store.state.dashboardData;
+let paginatedData = ref([]);
+let currentPage = ref(1);
+let perPage = ref(10);
+let filter = ref('');
+let pages = ref(Math.ceil(dashboardData.length / perPage.value));
+let showing = ref(0);
+
+onMounted(() => {paginateContent(dashboardData);});
+
+// miramos la cantidad de registros por pagina
+watch(perPage, (newVal, oldVal) => {
+    currentPage.value = 1;
+    pages.value = Math.ceil(dashboardData.length / perPage.value);
+    paginateContent(dashboardData);
+});
+
+// miramos la pagina actual
+watch(currentPage, (newVal, oldVal) => {
+    if (newVal === -1) {
+        currentPage.value = 1;
+    } else if (newVal === -2) {
+        currentPage.value = pages.value;
+    }
+    if (currentPage.value > pages.value) {
+        currentPage.value = pages.value;
+    }
+    if (currentPage.value < 1) {
+        currentPage.value = 1;
+    }
+    paginateContent(dashboardData);
+});
+
+// miramos si tenemos el filtro
+watch(filter, (newVal, oldVal) => {
+    currentPage.value = 1;
+    paginateContent(dashboardData, newVal);
+});
+
+
+// paginacion de datos
+function paginateContent(data, filter='') {
+    if (filter) {
+        data = filterData(data, filter);
+    }
+    let start = (currentPage.value - 1) * perPage.value;
+    let paginatedItems = data.slice(start, start + perPage.value);
+    showing.value = paginatedItems.length;
+    paginatedData.value = paginatedItems;
+}
+
+// filtro de datos
+function filterData(data, filter) {
+    let filteredData = data.filter((item) => {
+        return item.student.first_name.toLowerCase().includes(filter.toLowerCase()) || 
+        item.student.last_name.toLowerCase().includes(filter.toLowerCase()) || 
+        item.school.name.toLowerCase().includes(filter.toLowerCase()) || 
+        item.active_course.period.toLowerCase().includes(filter.toLowerCase()) || 
+        item.active_course.state.toLowerCase().includes(filter.toLowerCase()) || 
+        item.student.city.toLowerCase().includes(filter.toLowerCase());
+    });
+    return filteredData;
+}
+
+</script>
 <template>
-    <div class="mt-4 mx-4">
-        <div class="md:col-span-2 xl:col-span-3">
-            <h3 class="text-lg font-semibold">Mis Estudiantes</h3>
-        </div>
-        <div class="w-full overflow-hidden rounded-lg shadow-xs">
-            <div class="w-full overflow-x-auto">
-                <table class="w-full">
-                    <thead> 
-                        <tr
-                            class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                            <th class="px-4 py-3">Estudiante</th>
-                            <th class="px-4 py-3">Tipo</th>
-                            <th class="px-4 py-3">Nivel</th>
-                            <th class="px-4 py-3">Ciudad</th>
-                            <th class="px-4 py-3">Provincia</th>
+    <div
+        class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+        <div class="rounded-t mb-0 px-0 border-0">
+            <div class="flex flex-wrap items-center px-4 py-2">
+                <div class="relative w-full max-w-full flex-grow flex-1">
+                    <h3 class="text-gray-900">Mis Estudiantes 
+                    </h3>
+                </div>
+                <div class="relative w-full max-w-full flex-grow flex-1 text-right">
+                    <input type="text" class="input input-bordered input-xs" placeholder="buscar" v-model="filter">
+
+                </div>
+            </div>
+            <div class="block w-full overflow-x-auto">
+                <table class="table table-border">
+                    <thead>
+                        <tr class="bg-gray-200 text-center text-gray-950">
+                            <th>#</th>
+                            <th>Estudiante</th>
+                            <th>Colegio</th>
+                            <th>Periodo</th>
+                            <th>Estado</th>
+                            <th>Ubicación</th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-                        <tr
-                            class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                            <td class="px-4 py-3">
-                                <div class="flex items-center text-sm">
-                                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                                        <img class="object-cover w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                                            alt="" loading="lazy" />
-                                        <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                    </div>
-                                    <div>
-                                        <p class="font-semibold">ESTABAN FERNANDO AVILA PUMA</p>
-                                        <p class="text-xs text-gray-600 dark:text-gray-400">Colegio General Juan Flores
-                                        </p>
-                                    </div>
+                    <tbody>
+                        <tr v-for="row,idx in paginatedData" class=" hover:bg-yellow-50" :key="row" @click="emitIdStudent(row.student.id)">
+                            <td class="pb-0 pl-1">{{ (perPage*(currentPage-1)) + idx+1}}</td>
+                            <td class="pb-0 pl-1">{{row.student.first_name}} {{row.student.last_name}}</td>
+                            <td class="pb-0 pl-1">{{row.school.name}}</td>
+                            <td class="pb-0 pl-1">{{row.active_course.period}}</td>
+                            <td class="pb-0 pl-1">
+                                <div :class="classsStatus[row.active_course.state]">
+                                    {{row.active_course.state}}
                                 </div>
                             </td>
-                            <td class="px-4 py-3 text-sm">EGB</td>
-                            <td class="px-4 py-3 text-xs">
-                                <span
-                                    class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                                    8VO DE BASICA </span>
-                            </td>
-                            <td class="px-4 py-3 text-sm">LOS BANCOS</td>
-                            <td class="px-4 py-3 text-sm">PICHINCHA</td>
-                        </tr>
-                        <tr
-                            class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                            <td class="px-4 py-3">
-                                <div class="flex items-center text-sm">
-                                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                                        <img class="object-cover w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;facepad=3&amp;fit=facearea&amp;s=707b9c33066bf8808c934c8ab394dff6"
-                                            alt="" loading="lazy" />
-                                        <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                    </div>
-                                    <div>
-                                        <p class="font-semibold">CHRISTINA MARIA MELO SEGOVIA</p>
-                                        <p class="text-xs text-gray-600 dark:text-gray-400">Colegio Menor Marcelo Mendez</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-4 py-3 text-sm">BGU</td>
-                            <td class="px-4 py-3 text-xs">
-                                <span
-                                    class="px-2 py-1 font-semibold leading-tight text-yellow-700 bg-yellow-100 rounded-full">
-                                    PRIMER AÑO</span>
-                            </td>
-                            <td class="px-4 py-3 text-sm">NONO</td>
-                            <td class="px-4 py-3 text-sm">PICHINCHA</td>
-                        </tr> <tr
-                            class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                            <td class="px-4 py-3">
-                                <div class="flex items-center text-sm">
-                                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                                        <img class="object-cover w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                                            alt="" loading="lazy" />
-                                        <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                    </div>
-                                    <div>
-                                        <p class="font-semibold">CARLOS MANUEL SISALEMA RODRIGUEZ</p>
-                                        <p class="text-xs text-gray-600 dark:text-gray-400">Colego Gran Bretaña
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-4 py-3 text-sm">EGB</td>
-                            <td class="px-4 py-3 text-xs">
-                                <span
-                                    class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                                    8VO DE BASICA </span>
-                            </td>
-                            <td class="px-4 py-3 text-sm">QUITO</td>
-                            <td class="px-4 py-3 text-sm">PICHINCHA</td>
-                        </tr>
-                        <tr
-                            class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                            <td class="px-4 py-3">
-                                <div class="flex items-center text-sm">
-                                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                                        <img class="object-cover w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;facepad=3&amp;fit=facearea&amp;s=707b9c33066bf8808c934c8ab394dff6"
-                                            alt="" loading="lazy" />
-                                        <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                                    </div>
-                                    <div>
-                                        <p class="font-semibold">ESTEFANIA ALEXANDRA TRUJILLO MEDINA</p>
-                                        <p class="text-xs text-gray-600 dark:text-gray-400">Colegio Nacional Montufar</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-4 py-3 text-sm">BGE</td>
-                            <td class="px-4 py-3 text-xs">
-                                <span
-                                    class="px-2 py-1 font-semibold leading-tight text-yellow-700 bg-yellow-100 rounded-full">
-                                    10 DE BASICA</span>
-                            </td>
-                            <td class="px-4 py-3 text-sm">NONO</td>
-                            <td class="px-4 py-3 text-sm">PICHINCHA</td>
+                            <td class="pb-0 pl-1">{{row.student.city}}</td>
                         </tr>
                     </tbody>
-                </table>
+                </table> 
+                <br/>
+                <hr>
+                <div class="flex flex-wrap items-center px-4 py-2 bg-slate-100">
+                <div class="relative w-full max-w-full flex-grow flex-1 flex gap-1">
+                    <button 
+                        @click="currentPage =-1"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronDoubleLeftIcon class="w-4 h-4"/>
+                    </button>
+                    <button 
+                        @click="currentPage--"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronLeftIcon class="h-4 w-4"/>
+                    </button>
+                    <button 
+                        @click="currentPage++"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronRightIcon class="h-4 w-4"/>
+                    </button>
+                    <button 
+                        @click="currentPage=-2"
+                        class="btn btn-xs btn-outline text-gray-600">
+                        <ChevronDoubleRightIcon class="h-4 w-4"/>
+                    </button> 
+                </div>
+                <div class="relative text-xs">
+                    Página 1 de {{ pages }} - Mostrando {{ showing }} de {{ dashboardData.length }} Registros
+                </div>
+                <div class="relative w-full max-w-full flex-grow flex-1 text-right">
+                    <span class="text-xs">Elementos por Página </span>
+                    <select class="select select-bordered select-xs" v-model="perPage">
+                        <option value="10">10</option>
+                        <option value="30">30</option>
+                        <option value="60">60</option>
+                        <option value="100">100</option>        
+                    </select>
+                </div>
             </div>
-            <div
-                class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
-                <span class="flex items-center col-span-3"> Showing 21-30 of 100 </span>
-                <span class="col-span-2"></span>
-                <!-- Pagination -->
-                <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
-                    <nav aria-label="Table navigation">
-                        <ul class="inline-flex items-center">
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
-                                    aria-label="Previous">
-                                    <svg aria-hidden="true" class="w-4 h-4 fill-current" viewBox="0 0 20 20">
-                                        <path
-                                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                            clip-rule="evenodd" fill-rule="evenodd"></path>
-                                    </svg>
-                                </button>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">1</button>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">2</button>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 text-white dark:text-gray-800 transition-colors duration-150 bg-blue-600 dark:bg-gray-100 border border-r-0 border-blue-600 dark:border-gray-100 rounded-md focus:outline-none focus:shadow-outline-purple">3</button>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">4</button>
-                            </li>
-                            <li>
-                                <span class="px-3 py-1">...</span>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">8</button>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">9</button>
-                            </li>
-                            <li>
-                                <button
-                                    class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
-                                    aria-label="Next">
-                                    <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
-                                        <path
-                                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                            clip-rule="evenodd" fill-rule="evenodd"></path>
-                                    </svg>
-                                </button>
-                            </li>
-                        </ul>
-                    </nav>
-                </span>
+                <hr class="p-2">
             </div>
         </div>
     </div>
