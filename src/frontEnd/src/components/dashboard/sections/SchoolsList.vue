671b8a46418d5bef278c38620295487515e13db4
Eduardo Villota <eduardouio7@gmail.com>
2024-02-11 00:37:39 -0500

    agregamos el modal del colegio

--- src/frontEnd/src/components/dashboard/sections/SchoolsList.vue
+++ src/frontEnd/src/components/dashboard/sections/SchoolsList.vue
@@ -1,10 +1,28 @@
 <script setup>
 import TableSchools from '@/components/dashboard/TableSchools.vue';
+import ModalSchool from '@/components/dashboard/ModalSchool.vue';
 
+import { ref, onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+
+const store = useStore();
+let showModal = ref(false);
+let idSchool = ref('0');
+
+function handelModal() {
+    showModal.value = false;
+}
+
+function handleIdSchool(id){
+    idSchool.value = id.toString();
+    showModal.value = true;
+}
 </script>
 
 <template>
     <div>
         <TableSchools/>
+        <ModalSchool/>
     </div>
 </template>
