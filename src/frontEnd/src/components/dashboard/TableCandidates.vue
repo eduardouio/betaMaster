65d5412a6bbf1b753a04d1c8d9750c00490c7b73
Eduardo Villota <eduardouio7@gmail.com>
2024-02-12 22:22:22 -0500

    definido el borrador del tema

--- src/frontEnd/src/components/dashboard/TableCandidates.vue
+++ src/frontEnd/src/components/dashboard/TableCandidates.vue
@@ -1,17 +1,10 @@
-<template>
+    <template>
     <div
                         class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
                         <div class="rounded-t mb-0 px-0 border-0">
                             <div class="flex flex-wrap items-center px-4 py-2">
                                 <div class="relative w-full max-w-full flex-grow flex-1">
-                                    <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Candidatos      
-                                        <span class="bg-red-600 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded-full">2</span>    
-                                    </h3>
-                                </div>
-                                <div class="relative w-full max-w-full flex-grow flex-1 text-right">
-                                    <button
-                                        class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button">Ver Todo</button>
+                                    <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Mis Candidatos</h3>
                                 </div>
                             </div>
                             <div class="block w-full overflow-x-auto">
