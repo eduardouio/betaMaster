2e470375b8f924a4698813a4cc86151a7052f30a
Eduardo Villota <eduardouio7@gmail.com>
2024-03-20 17:06:28 -0500

    interaccion d carga de archivo pdf del curriculum

--- src/frontEnd/src/server-interactions.js
+++ src/frontEnd/src/server-interactions.js
@@ -61,6 +61,43 @@ const  serverInteractions = {
             };
         }
     },
+    async postFile(url, formData) {
+        let headers = serverConfigData.headers;
+        delete headers['Content-Type'];
+        console.dir(headers);
+        let response = await fetch(url,{
+            method: 'POST',
+            headers: headers,
+            body: formData,
+        });
+
+        if(response.status != 201){
+            let response_fail = await response.json();
+            return {
+                status: {
+                    is_success: false,
+                    message: 'Error al enviar la informacion, error en el servidor',
+                    url: url,
+                    response: response_fail,
+                },
+                response: null,
+            }
+        }else{
+            let response_success = await response.json();
+            return {
+                status:{
+                    is_success: true,
+                    message: 'Informacion enviada con exito',
+                    url: url,
+                    response: null,
+                },
+                response: response_success,
+            };
+        }
+
+
+
+    },
     async putData(url, data){
         let response = await fetch(url, {
             method: 'PUT',
