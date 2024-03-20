0a8a5a22da81e5e4008f4b06c3d7dcea5f12cd4f
Eduardo Villota <eduardouio7@gmail.com>
2024-03-20 18:00:45 -0500

    refactor

--- src/frontEnd/store.js
+++ src/frontEnd/store.js
@@ -34,7 +34,7 @@ export default createStore({
                 commit('setUserData', userData);
             }
             return userData;
-        }
+        },
     },
     getters: {
         getUserData(state) {
