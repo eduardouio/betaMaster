f889f38ad944da55ba27a720d150e4309f2b6871
Eduardo Villota <eduardouio7@gmail.com>
2024-03-22 18:13:53 -0500

    fin del dia

--- src/frontEnd/src/components/dashboard/ModalPasswordForm.vue
+++ src/frontEnd/src/components/dashboard/ModalPasswordForm.vue
@@ -1,31 +1,39 @@
 <script setup>
+
+import { XCircleIcon } from '@heroicons/vue/24/outline';
+
+const emits = defineEmits(['handelModalPassword']);
+
+function closeModal() {
+    emits('handelModalPassword');
+}
 </script>
 <template>
-    <div class="my-modal">
-        <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle" open="">
-            <div class="modal-box">
-                <h3 class="font-bold text-lg">Hello!</h3>
-                <p class="py-4">Press ESC key or click the button below to close</p>
-                <div class="modal-action">
-                    <form method="dialog">
-                        <!-- if there is a button in form, it will close the modal -->
-                        <button class="btn">Close</button>
-                    </form>
+    <dialog id="my_modal_5" class="modal bg-gray-100/90 text-sm md:text-md" open="">
+        <XCircleIcon @click="closeModal" class="w-5 h-5 text-red-600 hover:text-red-900 hover:border" />
+        <div class="modal-box p-2 w-6/12 border rounded-md border-sky-600 border-l-8">
+            <div class="flex">
+                <span class="w-full inline-block text-center text-sm text-cyan-800">
+                    Formulario De Cambio De Contraseña
+                </span>
+                <XCircleIcon @click="closeModal" class="w-5 h-5 text-red-600 hover:text-red-900 hover:border" />
+            </div>
+            <div class="mt-4 bg-slate-100 border">  
+                <div class="grid gap-1 text-sm grid-cols-1 md:grid-cols-3">
+                    <label for="country col-1">
+                      <strong class="text-red-500">*</strong>
+                      Pais
+                    </label>
+                    <input type="text" class=" col-2 input input-sm input-secondary focus:input-primary w-full md:h-11"
+                      placeholder="Sus Apellidos" />
                 </div>
             </div>
-        </dialog>
-    </div>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn" @click="closeModal">Close</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
 </template>
-<style scoped>
-.my-modal {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.8); /* Fondo semi-transparente */
-}
-</style>
