518c12dff26beb3eb270169ca6cecd058e046a12
Eduardo Villota <eduardouio7@gmail.com>
2024-04-03 03:30:07 -0500

    formulario de referencias personales

---
+++ src/frontEnd/src/components/dashboard/FormReferencesTeacher.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useStore } from 'vuex';
+const store = useStore();
+
+</script>
+<template>
+    <div>
+        <table class="table table-xs table-border 2xl:table-md">
+            
+        </table>
+    </div>
+</template>
