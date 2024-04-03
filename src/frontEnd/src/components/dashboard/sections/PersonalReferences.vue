3e342ce36e185df09368e4f8cc14f4537cf52052
Eduardo Villota <eduardouio7@gmail.com>
2024-04-03 03:30:27 -0500

    Tabla de referecias personales

--- src/frontEnd/src/components/dashboard/sections/PersonalReferences.vue
+++ src/frontEnd/src/components/dashboard/sections/PersonalReferences.vue
@@ -1,5 +1,51 @@
+<script setup>
+import { useStore, mapGetters } from 'vuex';
+import { ref } from 'vue';
+import { CheckIcon, PencilSquareIcon, XCircleIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+
+const store = useStore();
+const userData = store.state.userData;
+let showForm = ref(true);
+
+</script>
 <template>
-    <table class="table table-xs">
+<div>
+    <div v-if="showForm" class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="row">
+            <label for="enterprise">Empresa</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>
+        <div class="row">
+            <label for="enterprise">Fecha Incio</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>
+        <div class="row">
+            <label for="enterprise">Fecha Fin</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>  
+        <div class="row">
+            <label for="enterprise">Contacto</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>
+        <div class="row">
+            <label for="enterprise">Relación</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>
+        <div class="row">
+            <label for="enterprise">Tipo Referencia</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>
+        <div class="row">
+            <label for="enterprise">Correo</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>  
+        <div class="row">
+            <label for="enterprise">Teléfono</label>
+            <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
+        </div>  
+
+    </div>
+    <table v-else class="table table-xs">
         <!-- head -->
         <thead class="text-gray-600">
             <tr>
@@ -34,13 +80,5 @@
             </tr>
         </tbody>
     </table>
+</div>
 </template>
-<script setup>
-
-
-const props = defineProps({
-    references: Object,
-    
-})
-
-</script>
