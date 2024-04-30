129a25528081a1074f439cc4f03c6967b8589768
Eduardo Villota <eduardouio7@gmail.com>
2024-04-30 13:57:30 -0500

    finalmente logramos cargar la foto de perfil

--- src/backEnd/api/GenericsViews/UploadPictureFileAPIView.py
+++ src/backEnd/api/GenericsViews/UploadPictureFileAPIView.py
@@ -3,27 +3,25 @@ import re
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.parsers import FileUploadParser
 from accounts.models import CustomUserModel
-from django.core.files import File
-from django.core.files.base import ContentFile
 
 
-# /api/user/upload-picture/<int:pk>/<str:filename>/
+# /api/user/upload-picture/<int:pk>/
 class UploadPictureFileAPIView(APIView):
-    parser_classes = (FileUploadParser,)
 
-    def post(self, request, pk, filename, *args, **kwargs):
+    def post(self, request, pk, *args, **kwargs):
         file_obj = request.FILES['file']
         user = CustomUserModel.objects.get(pk=pk)
-        if not re.search(r'\.(png|jpg|jpeg|gif|bmp|tiff|svg)$', filename, re.IGNORECASE):
+        if not re.search(
+            r'\.(png|jpg|jpeg|gif|bmp|tiff|svg)$', file_obj.name, re.IGNORECASE
+        ):
             return Response({
                 'message': 'Archivo no permitido',
                 'status': 'error',
                 'url': ''
             }, status=status.HTTP_400_BAD_REQUEST)
         user.picture.delete()
-        filename = str(user.pk) + filename[filename.rfind('.'):]
+        filename = str(user.pk) + file_obj.name[file_obj.name.rfind('.'):]
         user.picture.save(filename, file_obj.file, save=True)
         return Response({
             'message': 'Archivo subido correctamente',
