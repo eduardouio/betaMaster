45348439fe108eb8c4b5d78a811856621eee85af
Eduardo Villota <eduardouio7@gmail.com>
2024-04-30 16:57:42 -0500

    de momento eliminamos el adjunto de la referencia

--- src/frontEnd/src/components/dashboard/sections/PersonalReferences.vue
+++ src/frontEnd/src/components/dashboard/sections/PersonalReferences.vue
@@ -1,20 +1,52 @@
 <script setup>
 import { useStore } from 'vuex';
 import { onUnmounted, ref } from 'vue';
-import { CheckIcon, PencilSquareIcon, XCircleIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+import { 
+    CheckIcon, PencilSquareIcon, XCircleIcon, XMarkIcon, PlusCircleIcon,
+    FolderArrowDownIcon
+} from '@heroicons/vue/24/outline';
 import userRegisterFormVue from '../../userRegisterForm.vue';
 
 const store = useStore();
 const references = store.getters.getReferences;
+const showForm = ref(false);
 
 const deleteReferece = function (idx) {
     store.dispatch('deleteReference', idx);
 };
 
+const addReference = function () {
+    showForm.value = !showForm.value;
+};
+
+const currentReference = ref({
+    
+});
+
+const setRerence = function (reference) {
+    currentReference.value = reference
+
+};
+
+
 </script>
 <template>
 <div>
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="flex justify-between items-center p-2">
+        <button v-if="!showForm"  @click="showForm = !showForm" class="btn btn-sm bg-lime-600 text-white">
+            <PlusCircleIcon class="w-5 h-5" />
+            Agregar Referencia
+        </button>
+        <button v-else @click="addReference" class="btn btn-sm bg-slate-600 text-white">
+            <FolderArrowDownIcon class="w-5 h-5" />
+            Guardar Referencia
+        </button>
+        <button v-if="showForm" @click="showForm = !showForm" class="btn btn-sm bg-orange-600 text-white">
+            <XCircleIcon class="w-5 h-5" />
+            Cancelar
+        </button>
+    </div>
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4" v-if="showForm">
         <div class="row">
             <label for="enterprise">Empresa</label>
             <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
@@ -48,7 +80,7 @@ const deleteReferece = function (idx) {
             <input type="text" id="enterprise" class="input input-sm input-secondary focus:input-primary w-full md:h-11" />
         </div>  
     </div>
-    <table class="table table-xs">
+    <table class="table table-xs" v-else>
         <!-- head -->
         <thead class="text-gray-600">
             <tr>
@@ -76,7 +108,7 @@ const deleteReferece = function (idx) {
                 </td>
                 <td>
                     <span class="flex items-center gap-2">
-                        <PencilSquareIcon class="w-5 h-5 text-primary" />
+                        <PencilSquareIcon class="w-5 h-5 text-primary" @click="setRerence(exp)"/>
                         <XMarkIcon class="w-5 h-5 text-error" />
                     </span>
                 </td>
