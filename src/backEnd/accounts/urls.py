7fe9d8d29dd7dcb70445fcb40345a5b3d5335e8e
Eduardo Villota <eduardouio7@gmail.com>
2024-05-17 16:22:05 -0500

    inicio de sesion

--- src/backEnd/accounts/urls.py
+++ src/backEnd/accounts/urls.py
@@ -3,7 +3,7 @@ from accounts.views import LoginTV, HomeRV, LogoutRV
 
 app_name = "accounts"
 urlpatterns = [
+    path('', HomeRV.as_view(), name='home'),
     path('login/', LoginTV.as_view(), name='login'),
     path('logout/', LogoutRV.as_view(), name='logout'),
-    path('home/', HomeRV.as_view(), name='home'),
 ]
--- src/backEnd/accounts/views/LogoutRV.py
+++ src/backEnd/accounts/views/LogoutRV.py
@@ -5,8 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # /accounts/logout/
-class LogoutRV(RedirectView):
-    url = '/'
+class LogoutRV(LoginRequiredMixin, RedirectView):
+    login_url = '/accounts/login/'
+    url = '/accounts/login/'
+
     def get(self, request, *args, **kwargs):
         logout(request)
         return HttpResponseRedirect(self.get_redirect_url())
