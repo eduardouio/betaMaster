3b3f48dc9174bded5458658375d540b3f43dcbab
Eduardo Villota <eduardouio7@gmail.com>
2024-05-18 05:53:15 -0500

    Refactorizar nombres de rutas en el enrutador

--- src/frontEnd/teacher_dashboard/src/router/index.js
+++ src/frontEnd/teacher_dashboard/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import RegisterView from '../views/RegisterView.vue';
-import DashBoardTeacher from '../views/DashBoardTeacher.vue';
-import HomeDashboard from '../components/dashboard/sections/HomeDashboard.vue';
-import SchoolsList from '../components/dashboard/sections/SchoolsList.vue';
-import StudentsList from '../components/dashboard/sections/StudentsList.vue';
-import TeacherProfile from '../components/dashboard/sections/TeacherProfile.vue';
-import TeacherProfileForm from '../components/dashboard/sections/TeacherProfileForm.vue';
-import LoginView from '../views/LoginView.vue';
+import HomeView from '@/views/HomeView.vue';
+import RegisterView from '@/views/RegisterView.vue';
+import DashBoardTeacher from '@/views/DashBoardTeacher.vue';
+import HomeDashboard from '@/components/dashboard/sections/HomeDashboard.vue';
+import SchoolsList from '@/components/dashboard/sections/SchoolsList.vue';
+import StudentsList from '@/components/dashboard/sections/StudentsList.vue';
+import TeacherProfile from '@/components/dashboard/sections/TeacherProfile.vue';
+import TeacherProfileForm from '@/components/dashboard/sections/TeacherProfileForm.vue';
+import LoginView from '@/views/LoginView.vue';
 
 
 const router = createRouter({
