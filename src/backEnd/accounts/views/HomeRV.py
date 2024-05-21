d91bb555c41e1c6f7e42914d28c72af6c20b48a5
Eduardo Villota <eduardouio7@gmail.com>
2024-05-21 05:59:06 -0500

    vista de redireccion de home de usuarios

--- src/backEnd/accounts/views/HomeRV.py
+++ src/backEnd/accounts/views/HomeRV.py
@@ -1,10 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-# /accounts/home/
-class HomeRV(TemplateView):
-    template_name = 'accounts/home.html'
+class HomeRV(LoginRequiredMixin, RedirectView):
+    login_url = '/accounts/login/'
+    url = '/admin/'
 
     def get(self, request, *args, **kwargs):
+        user = request.user
+        import ipdb; ipdb.set_trace()
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
