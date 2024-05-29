567456aa82b85a0076e989a7a024f62c8ec8b15d
Eduardo Villota <eduardouio7@gmail.com>
2024-05-29 05:26:51 -0500

    el home rv cuando en raiz esta logeado

--- src/backEnd/homeSchool/urls.py
+++ src/backEnd/homeSchool/urls.py
@@ -27,7 +27,7 @@ admin.site.site_header = ''
 admin.site.site_description = 'Sistema de Escuela En Casa'
 
 urlpatterns = [
-    # path('home-rv/', HomeRV.as_view(), name='home'),
+    path('home-rv/', HomeRV.as_view(), name='home'),
     path('', HomeRV.as_view(), name='home'),
     path('dashboard-profesor/', DashboardTeacherTV.as_view(), name='dashboard-teacher'),
     path('admin/', admin.site.urls),
