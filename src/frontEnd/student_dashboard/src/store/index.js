4fa5c991e79640d6b5abb08d975486d3ce00fa88
Eduardo Villota <eduardouio7@gmail.com>
2024-05-29 13:18:22 -0500

    chore: Remove unused store modules and update dependencies

--- src/frontEnd/student_dashboard/src/store/index.js
+++ src/frontEnd/student_dashboard/src/store/index.js
@@ -1,7 +1,4 @@
 import { createStore } from "vuex";
-import references from "@/store/modules/references.js";
-import students from "@/store/modules/students.js";
-import bankAccounts from "@/store/modules/bankAccounts.js";
 import profile from "@/store/modules/profile.js";
 import scholls from "@/store/modules/schools.js";
 import courses from "@/store/modules/courses.js";
@@ -41,9 +38,6 @@ const store =  createStore({
   },
   modules: {
     profile,
-    bankAccounts,
-    students,
-    references,
     scholls,
     courses,
   },
